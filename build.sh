a8f625d6e84bd270d7e0584bbd4ca0b464895933
Roman Khadraliev <Romch33tos@yandex.ru>
2026-09-24 09:10:54 +0700

    chore: реализована очистка и компиляция проекта через kotlinc

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ if [ ! -f "${KOTLINX_CLI_JAR}" ]; then
   echo "Не найден ${KOTLINX_CLI_JAR}. Скачайте kotlinx-cli-jvm-0.3.5.jar в каталог lib/"
   exit 1
 fi
+
+rm -f "${JAR_NAME}"
+
+kotlinc "${SRC_DIR}" -cp "${KOTLINX_CLI_JAR}" -include-runtime -d "${JAR_NAME}"
+
+echo "Сборка завершена: ${JAR_NAME}"
